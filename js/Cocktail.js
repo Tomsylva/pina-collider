bfd37af73e684bca64466d7cbd97a3801de009bd
Tomsylva <Tomsylvanz@gmail.com>
2021-02-16 20:34:54 +0100

    powerups now dissapear upon collision

--- js/Cocktail.js
+++ js/Cocktail.js
@@ -1,22 +1,21 @@
 class Cocktail {
+  constructor() {
+    this.x = WIDTH;
+    this.width = 60;
+    this.height = 60;
+    this.y = 330;
+  }
 
-    constructor(){
-        this.x = WIDTH;
-        this.width = 60;
-        this.height = 60;
-        this.y = 330; 
+  draw() {
+    // This ensure the coconuts fall in the same "y" position
+    if (keyIsDown(39)) {
+      this.x -= 8;
     }
 
-    draw() {
-        // This ensure the coconuts fall in the same "y" position
-        if(keyIsDown(39)){
-            this.x -=8;
-        }
-
-        if(keyIsDown(37)){
-            this.x +=8;
-        }
-
-        image (cocktailImage, this.x, this.y, this.width, this.height);
+    if (keyIsDown(37)) {
+      this.x += 8;
     }
+
+    image(cocktailImage, this.x, this.y, this.width, this.height);
+  }
 }
--- js/Game.js
+++ js/Game.js
@@ -1,148 +1,147 @@
-                // SAVE SCORE
-                // CREATE BUTTON TO PLAY AGAIN
-                // RESET GAME
-
+// SAVE SCORE
+// CREATE BUTTON TO PLAY AGAIN
+// RESET GAME
 
 class Game {
+  constructor() {
+    this.background = new Background();
+    this.bar = new Bar();
+    this.sharks = [];
+    this.coconuts = [];
+    this.cocktails = [];
+    this.brewskis = [];
+    this.player = new Player();
+    this.score = 0;
+  }
 
-    constructor(){
-        this.background = new Background;
-        this.bar = new Bar;
-        this.sharks = [];
-        this.coconuts = [];
-        this.cocktails = [];
-        this.brewskis = [];
-        this.player = new Player;
-        this.score = 0;
+  setup() {}
+
+  draw() {
+    clear();
+
+    if (keyIsDown(39)) {
+      this.score++;
     }
 
-    setup(){
-
+    if (keyIsDown(37)) {
+      this.score--;
     }
 
-    draw(){
+    document.querySelector("h1 span").innerText = Math.floor(this.score / 10);
 
-        clear();
+    this.background.draw();
+    this.bar.draw();
+    this.player.draw();
 
-        if(keyIsDown(39)){
-            this.score ++;
+    // SHARK LOGIC
+
+    if (frameCount % Math.floor(random(5000)) === 0) {
+      this.sharks.push(new Shark());
+    }
+
+    this.sharks.forEach((shark, index) => {
+      shark.draw();
+
+      if (shark.x + shark.width <= 0) {
+        this.sharks.splice(index, 1);
+      }
+
+      if (this.collisionCheck(this.player, shark)) {
+        noLoop();
+        const button = document.createElement("button");
+        button.innerText = "Play again?";
+        document.body.appendChild(button);
+        button.onclick = () => {
+          //ADD REFRESH FUNCTION
+          restartGame();
+          button.parentNode.removeChild(button);
         };
+      }
+    });
 
-        if(keyIsDown(37)){
-            this.score --;
+    // COCONUT LOGIC
+
+    if (frameCount % Math.floor(random(200)) === 0) {
+      this.coconuts.push(new Coconut());
+    }
+
+    this.coconuts.forEach((coconut, index) => {
+      coconut.draw();
+
+      if (coconut.x + coconut.width <= 0) {
+        this.coconuts.splice(index, 1);
+      }
+
+      if (this.collisionCheck(this.player, coconut)) {
+        noLoop();
+        const button = document.createElement("button");
+        button.innerText = "Play again?";
+        document.body.appendChild(button);
+        button.onclick = () => {
+          //ADD REFRESH FUNCTION
+          restartGame();
+          button.parentNode.removeChild(button);
+        };
+      }
+    });
+
+    //POWERUPS
+
+    if (frameCount % Math.floor(random(5000)) === 0) {
+      this.cocktails.push(new Cocktail());
+    }
+    this.cocktails.forEach((cocktail, index) => {
+      cocktail.draw();
+      if (cocktail.x + cocktail.width <= 0) {
+        this.cocktails.splice(index, 1);
+      }
+      if (this.collisionCheck(this.player, cocktail)){
+        this.cocktails.splice(index, 1);
+    }
+    });
+
+    if (frameCount % Math.floor(random(5000)) === 0) {
+      this.brewskis.push(new Beer());
+    }
+    this.brewskis.forEach((beer, index) => {
+        beer.draw();
+        if (beer.x + beer.width <= 0) {
+          this.brewskis.splice(index, 1);
         }
+        if (this.collisionCheck(this.player, beer)){
+          this.brewskis.splice(index, 1);
+      }
+      });
+  }
 
-        document.querySelector("h1 span").innerText = Math. floor(this.score / 10);
-
-        this.background.draw();
-        this.bar.draw();
-        this.player.draw();
-
-        // SHARK LOGIC
-
-        if (frameCount % Math.floor(random(5000)) === 0){
-            this.sharks.push(new Shark());
-        } 
-
-        this.sharks.forEach((shark, index) => {
-            shark.draw(); 
-
-            if(shark.x + shark.width <= 0){
-                this.sharks.splice(index, 1);
-            }
-
-            if(this.collisionCheck(this.player, shark)){
-                noLoop();
-                const button = document.createElement("button");
-                button.innerText = "Play again?"
-                document.body.appendChild(button);
-                button.onclick = () => {
-                    //ADD REFRESH FUNCTION
-                    restartGame();
-                    button.parentNode.removeChild(button);
-                }
-            }
-
-        });
-
-        // COCONUT LOGIC
-
-        if(frameCount % Math.floor(random(200)) === 0){
-            this.coconuts.push(new Coconut());
-        }
-        
-        this.coconuts.forEach((coconut, index) => {
-            coconut.draw();
-
-            if(coconut.x + coconut.width <= 0){
-                this.coconuts.splice(index, 1);
-            }
-
-            if(this.collisionCheck(this.player, coconut)){
-                noLoop();
-                const button = document.createElement("button");
-                button.innerText = "Play again?"
-                document.body.appendChild(button);
-                button.onclick = () => {
-                    //ADD REFRESH FUNCTION
-                    restartGame();
-                    button.parentNode.removeChild(button);
-                }
-            }
-        })
-
-        //POWERUPS
-
-        if (frameCount % Math.floor(random(5000)) === 0){
-            this.cocktails.push(new Cocktail());
-        }
-        this.cocktails.forEach((cocktail, index) => {
-            cocktail.draw();
-            if(cocktail.x + cocktail.width <= 0){
-                this.cocktails.splice(index, 1);
-            }
-        })
-        if (frameCount % Math.floor(random(5000)) === 0){
-            this.brewskis.push(new Beer());
-        }
-        this.brewskis.forEach((beer, index) => {
-            beer.draw();
-            if(beer.x + beer.width <= 0){
-                this.brewskis.splice(index, 1);
-            }
-        })
-     }
-
-    collisionCheck(player, obstacle){
-
-        //   player.left + player.width (players.rightSide)
+  collisionCheck(player, obstacle) {
+    //   player.left + player.width (players.rightSide)
     //  if player's right side is to the left of the obstacle's left
     if (player.x + player.width < obstacle.x) {
-        return false;
-      }
-  
-      //  obstacle's left and obstacle width (obstacle.rightSide)
-      // if obstacle's right side is to the left of player's left
-      if (obstacle.x + obstacle.width < player.x) {
-        return false;
-      }
-  
-      // player.topSide > obstacle.TopSide + obstacle.height (obstacle.Bottom)
-      // player top side is below obstacle's bottom side
-      if (player.y > obstacle.y + obstacle.height) {
-        return false;
-      }
-  
-      //  obstacle.topSide > player.topSide + player.height (player.bottomSide)
-      //  obstacle top side is below the player's bottom side
-      if (obstacle.y > player.y + player.height) {
-        return false;
-      }
-      return true;
+      return false;
     }
 
-    keyIsPressed(){
-        this.player.keyIsPressed();
+    //  obstacle's left and obstacle width (obstacle.rightSide)
+    // if obstacle's right side is to the left of player's left
+    if (obstacle.x + obstacle.width < player.x) {
+      return false;
     }
 
+    // player.topSide > obstacle.TopSide + obstacle.height (obstacle.Bottom)
+    // player top side is below obstacle's bottom side
+    if (player.y > obstacle.y + obstacle.height) {
+      return false;
+    }
+
+    //  obstacle.topSide > player.topSide + player.height (player.bottomSide)
+    //  obstacle top side is below the player's bottom side
+    if (obstacle.y > player.y + player.height) {
+      return false;
+    }
+    return true;
+  }
+
+  keyIsPressed() {
+    this.player.keyIsPressed();
+  }
 }
